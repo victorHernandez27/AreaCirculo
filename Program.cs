01dd24818e09f2966222b608b3cf7da8d581b7fc
Victor Hernández <victor.hhernandez27@gmail.com>
2022-03-07 15:07:25 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AreaCirculo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float raio;
+            
+
+            Console.WriteLine("Digite o valor do raio:");
+            raio = float.Parse(Console.ReadLine());
+
+            if(raio <= 0)
+            {
+                Console.WriteLine("Não ha valor possível para calcular uma área");
+            }
+            else
+            {                
+                Console.WriteLine("A area do circulo é: {0}", (3.1415 * (raio * raio)));
+            }
+
+        }
+    }
+}
